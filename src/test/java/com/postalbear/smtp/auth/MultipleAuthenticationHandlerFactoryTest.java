8a4dc2dbd6007404d98c66881e322408b8226fb6
PostalBear <grigory.fadeev@gmail.com>
2015-05-19 22:22:09 +0200

    Test coverage improve
    AuthenticationHandlerFactories

--- src/test/java/com/postalbear/smtp/auth/MultipleAuthenticationHandlerFactoryTest.java
+++ src/test/java/com/postalbear/smtp/auth/MultipleAuthenticationHandlerFactoryTest.java
@@ -2,6 +2,8 @@
  */
 package com.postalbear.smtp.auth;
 
+import com.postalbear.smtp.SmtpSession;
+import com.postalbear.smtp.exception.SmtpException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +13,9 @@ import org.mockito.runners.MockitoJUnitRunner;
 import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
@@ -21,19 +26,39 @@ public class MultipleAuthenticationHandlerFactoryTest {
 
     @Mock
     private AuthenticationHandlerFactory mockedFactoryDelegate;
+    @Mock
+    private SmtpSession session;
 
     private MultipleAuthenticationHandlerFactory factory;
 
     @Before
-    public void init() {
+    public void init() throws Exception {
         //to check that mechanisms will be converted to upper case
         when(mockedFactoryDelegate.getAuthenticationMechanisms()).thenReturn(Collections.singleton("plain"));
         factory = new MultipleAuthenticationHandlerFactory(mockedFactoryDelegate);
     }
 
     @Test
-    public void testGetAuthenticationMechanisms() {
+    public void testGetAuthenticationMechanisms() throws Exception {
         assertEquals(Collections.singleton("PLAIN"), factory.getAuthenticationMechanisms());
+    }
+
+    @Test
+    public void testDumbCreate() throws Exception {
+        AuthenticationHandler handler = mock(AuthenticationHandler.class);
+        when(mockedFactoryDelegate.create(eq("PLAIN"), eq(session))).thenReturn(handler);
+        assertSame(handler, factory.create("PLAIN", session));
+    }
+
+    @Test(expected = SmtpException.class)
+    public void testCreateWithUnknownMechanism() throws Exception {
+        try {
+            factory.create("UNKNOWN", session);
+        } catch (SmtpException ex) {
+            assertEquals(504, ex.getResponseCode());
+            assertEquals("5.5.4 The requested authentication mechanism is not supported", ex.getResponseMessage());
+            throw ex;
+        }
     }
 
     @Test(expected = NullPointerException.class)
---
+++ src/test/java/com/postalbear/smtp/auth/login/LoginAuthenticationHandlerFactoryTest.java
@@ -0,0 +1,52 @@
+package com.postalbear.smtp.auth.login;
+
+import com.postalbear.smtp.SmtpSession;
+import com.postalbear.smtp.auth.CredentialsValidator;
+import com.postalbear.smtp.exception.SmtpException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * This is quite dumb test, it checks basic negative cases.
+ * More deep handler testing is subject for other tests.
+ *
+ * @author Grigory Fadeev.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class LoginAuthenticationHandlerFactoryTest {
+
+    @Mock
+    private CredentialsValidator credentialsValidator;
+    @Mock
+    private SmtpSession session;
+    @InjectMocks
+    private LoginAuthenticationHandlerFactory factory;
+
+    @Test
+    public void testGetAuthenticationMechanisms() throws Exception {
+        assertEquals(Collections.singleton("LOGIN"), factory.getAuthenticationMechanisms());
+    }
+
+    @Test(expected = SmtpException.class)
+    public void testCreateWithUnknownMechanism() throws Exception {
+        try {
+            factory.create("UNKNOWN", session);
+        } catch (SmtpException ex) {
+            assertEquals(504, ex.getResponseCode());
+            assertEquals("5.5.4 The requested authentication mechanism is not supported", ex.getResponseMessage());
+            throw ex;
+        }
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testNullCredentialValidator() throws Exception {
+        new LoginAuthenticationHandlerFactory(null);
+    }
+}
