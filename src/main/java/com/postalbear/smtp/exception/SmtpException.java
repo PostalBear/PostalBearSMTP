741231bfc5d7e93e203ba0126e1147f2f2570e48
PostalBear <grigory.fadeev@gmail.com>
2015-05-19 22:21:16 +0200

    Add javadoc

--- src/main/java/com/postalbear/smtp/exception/SmtpException.java
+++ src/main/java/com/postalbear/smtp/exception/SmtpException.java
@@ -11,19 +11,34 @@ public class SmtpException extends RuntimeException {
     private final int responseCode;
     private final String responseMessage;
 
+    /**
+     * Constructs instance of SmtpException.
+     *
+     * @param responseCode    resulting SMTP code
+     * @param responseMessage diagnostic message
+     */
     public SmtpException(int responseCode, String responseMessage) {
         this.responseCode = responseCode;
         this.responseMessage = responseMessage;
     }
 
+    /**
+     * @return SMTP code
+     */
     public int getResponseCode() {
         return responseCode;
     }
 
+    /**
+     * @return diagnostic message
+     */
     public String getResponseMessage() {
         return responseMessage;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getMessage() {
         return getResponseCode() + " " + getResponseMessage();
